73f8b0108f507277a89d76c3e5b71de6cf8bc4bd
Chance Gardner <Chantz465@gmail.com>
2022-04-22 13:05:22 -0500

    (add)

---
+++ Exercis2/Exercis2/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace Exercis2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            var name = Console.ReadLine();
+
+            var result = Add(10, 20);
+
+        }
+
+
+        public static int Add(int num1, int num2)
+
+        {
+            var sum = num1 + num2;
+            return sum;
+
+
+        }
+
+        public static int Multiply(int num1, int num2)
+
+        {
+            var times = num1 * num2;
+            return times;
+
+        }
+
+
+
+
+
+
+
+
+
+
+    }
+}
